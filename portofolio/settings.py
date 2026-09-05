f44045580d202894686918077aedfad47010e4db
nalanii-c <chandra.nala20@gmail.com>
2026-09-05 22:14:16 +0700

    Ubah production ke false dan update requirements

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khalisha-nalani-myportofolio.pws.cs.ui.ac.id"]
-PRODUCTION = os.getenv('PRODUCTION', 'True').lower() == 'true'
+PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
