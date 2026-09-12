6ba642e63c1941bdb172a3db42a52483fd12ec4d
nalanii-c <chandra.nala20@gmail.com>
2026-09-13 00:32:31 +0700

    feat: update named routes in main/urls.py for Tugas 2

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from main.views import show_main, show_experience, show_skills
+
+from main.views import show_main, show_education, show_experience, show_skills
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
+
+    path("education/", show_education, name="show_education"),
+
     path("experience/", show_experience, name="show_experience"),
+
     path("skills/", show_skills, name="show_skills"),
 ]
