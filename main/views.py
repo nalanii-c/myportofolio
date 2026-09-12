884f625f50551a08145038a12a2ac0effce61d3e
nalanii-c <chandra.nala20@gmail.com>
2026-09-13 00:34:07 +0700

    fix: resolve merge conflict in main views

--- main/views.py
+++ main/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
-from main.models import Skill, Experience
+from main.models import Experience, Skill
 
-<<<<<<< HEAD
-#Main
-=======
-
->>>>>>> dd5a78b (fix: restructure main app folder and add new model migrations)
 def show_main(request):
     context = {
         "name": "Khalisha Nalani Chandra",
         "npm": "2506625041",
         "study_program": "S1 Sistem Informasi",
-        "bio": (
-            "Second-Year Information Systems Undergraduate @Universitas Indonesia | Graphic Designer | Data Science Enthusiast | Figure Skater "
-        ),
+        "bio": "Second-Year Information Systems Undergraduate @Universitas Indonesia | Graphic Designer | Data Science Enthusiast | Figure Skater",
     }
     return render(request, "index.html", context)
 
-#Experience
+def show_education(request):
+    context = {
+        "name": "Khalisha Nalani Chandra",
+    }
+    return render(request, "education.html", context)
+
+def show_skills(request):
+    context = {
+        "name": "Khalisha Nalani Chandra",
+        "skills_list": Skill.objects.all(),
+    }
+    return render(request, "skills.html", context)
+
 def show_experience(request):
     context = {
         "name": "Khalisha Nalani Chandra",
@@ -25,14 +30,8 @@ def show_experience(request):
     }
     return render(request, "experience.html", context)
 
-<<<<<<< HEAD
-#Skill
-=======
-#Skills
->>>>>>> dd5a78b (fix: restructure main app folder and add new model migrations)
-def show_skills(request):
+def show_projects(request):
     context = {
         "name": "Khalisha Nalani Chandra",
-        "skills_list": Skill.objects.all(),
     }
-    return render(request, "skills.html", context)
+    return render(request, "project.html", context)
