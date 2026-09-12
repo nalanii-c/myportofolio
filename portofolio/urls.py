dfd54b121dbe09a5233ea6d3994d455d47cff2c9
nalanii-c <chandra.nala20@gmail.com>
2026-09-12 20:54:51 +0700

    chore: sync local changes

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import include, path
-=======
 ﻿from django.urls import path
-from main.views import show_main, show_experience 
 
-app_name = 'main'
->>>>>>> 3dd9ccd (fix: update experience template layout and styling)
+from main.views import show_main, show_experience
+
+app_name = "main"
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('experience/', show_experience, name='show_experience'),
+    path("", show_main, name="show_main"),
+    path("experience/", show_experience, name="show_experience"),
 ]
