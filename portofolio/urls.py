1047bbb916ec3348f4a317716cba1432088b684e
nalanii-c <chandra.nala20@gmail.com>
2026-09-12 20:33:27 +0700

    fix: update experience template layout and styling

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import include, path
+=======
+﻿from django.urls import path
+from main.views import show_main, show_experience 
+
+app_name = 'main'
+>>>>>>> 3dd9ccd (fix: update experience template layout and styling)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("main.urls")),
+    path('', show_main, name='show_main'),
+    path('experience/', show_experience, name='show_experience'),
 ]
---
+++ portofolio/views.py
@@ -0,0 +1,19 @@
+﻿from django.shortcuts import render
+from .models import Experience
+
+def show_main(request):
+    context = {
+        'name': 'Khalisha Nalani Chandra',
+        'npm': '2506625041',
+        'study_program': 'Information Systems',
+        'bio': 'Second-Year Information Systems Undergraduate @Universitas Indonesia | Graphic Designer | Data Science Enthusiast | Figure Skater',
+    }
+    return render(request, 'index.html', context)
+
+def show_experience(request):
+    experience_list = Experience.objects.all()
+    context = {
+        'name': 'Khalisha Nalani Chandra',
+        'experience_list': experience_list,
+    }
+    return render(request, 'experience.html', context)
