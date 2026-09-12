652622569097311a19d8b949649781a954664fe6
nalanii-c <chandra.nala20@gmail.com>
2026-09-12 22:17:46 +0700

    feat: register named routes on main/urls.py with distinct URLs for Tugas 2

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -1,6 +1,7 @@
 ﻿from django.contrib import admin
 from django.urls import path, include
 
+# Mendaftarkan Nama Routesnya
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
