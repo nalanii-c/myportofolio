52ae8b96ed186f75b20de558d0b89d95d492b4c6
nalanii-c <chandra.nala20@gmail.com>
2026-09-13 01:01:32 +0700

    fix: remove duplicate files and add additional HTML templates

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
-
-from main.views import show_main, show_education, show_experience, show_skills
+from main.views import show_main, show_education, show_experience, show_skills, show_projects
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
-
     path("education/", show_education, name="show_education"),
-
     path("experience/", show_experience, name="show_experience"),
-
     path("skills/", show_skills, name="show_skills"),
+    path("projects/", show_projects, name="show_projects"),
 ]
